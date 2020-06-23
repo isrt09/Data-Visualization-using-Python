f193b4754df2c736aa1c9dac9d2b14a1bf6d653f
codexninja <md_maz82@yahoo.com>
2020-06-23 15:20:28 +0600

    matplotlib file is initialized...

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Data-Visualization-using-Python
+Some libraries will be focused as like matplotlib and seaborn
---
+++ matplotlib.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot as plt
+import numpy  as np
+import pandas as pd
+
+x = [1, 2, 3, 4, 5]
+y = [5, 7, 3, 8, 4]
+plt.bar(x,y)
+plt.show()
