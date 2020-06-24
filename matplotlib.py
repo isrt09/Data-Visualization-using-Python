ddb678249046ef71021014fe78136daa2ae24ca7
Mazedur <mazedur@gmail.com>
2020-06-24 23:51:42 +0600

    updated by some labels

--- matplotlib.py
+++ matplotlib.py
@@ -4,5 +4,9 @@ import pandas as pd
 
 x = [1, 2, 3, 4, 5]
 y = [5, 7, 3, 8, 4]
+plt.title('Title text here ...')
+plt.xlabels('X Axis Label')
+plt.ylabels('Y Axis Label')
+plt.grid(TRUE)
 plt.bar(x,y)
 plt.show()
