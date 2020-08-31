85f6b97fa270f60ede4e8d6c61bc7726a9a10bf0
Mazedur <mazedur@gmail.com>
2020-09-01 02:12:55 +0600

    Pie Plotted

---
+++ Day02/PieChart.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+x = [50,20,10,15,5]
+label = ['HTML','JS','CSS','JQ','PHP']
+pieexplode = [0.3,0,0,0,0]
+plt.pie(x, labels = label, explode=pieexplode)
+plt.show()
