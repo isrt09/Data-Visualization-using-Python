d2c2007c7ce84bc61db51faf3bba0be14df34959
Mazedur <mazedur@gmail.com>
2020-09-01 15:00:05 +0600

    Grid Lines is plotted

--- Day02/PieChart.py
+++ Day02/PieChart.py
@@ -3,5 +3,5 @@ import matplotlib.pyplot as plt
 x = [50,20,10,15,5]
 label = ['HTML','JS','CSS','JQ','PHP']
 pieexplode = [0.3,0,0,0,0]
-plt.pie(x, labels = label, explode=pieexplode)
+plt.pie(x, labels = label, explode=pieexplode, startangle=90)
 plt.show()
