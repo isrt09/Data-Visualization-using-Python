b278911dfe83edeec2f085a2be63964138c01b63
Mazedur <mazedur@gmail.com>
2020-09-16 22:40:32 +0600

    Dataset added

--- Day02/GridLine.py
+++ Day02/GridLine.py
@@ -1,3 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+x1 = [1,2,3,4,5]
+y1 = [1,2,4,8,16]
+
+plt.plot(x1,y1,'r--', label='Students')
+plt.title('Demo Title')
+plt.xlabel('X-Axis Label')
+plt.ylabel('Y-Axis Label')
+plt.legend(loc='best')
+plt.grid(ls='-', color='black')
+plt.grid(which='major', ls='-', color='black')
+plt.minorticks_on()
+plt.grid(which='minor', ls='--', color='grey')
+plt.show()
